9cbb401d75309657354553ac2607ba625873d374
bit2swaz <bit2swaz@gmail.com>
2025-10-08 14:54:29 +0530

    wrapped up 'structs, methods and interfaces

---
+++ shapes/shapes.go
@@ -0,0 +1,41 @@
+package main
+
+import "math"
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.base * t.height
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.height * rectangle.width
+}
